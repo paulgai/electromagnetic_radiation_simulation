d23fa9dd6691b95a33a6e9f73fc1d06ff6e7059f
marista93 <maria.st.bel@gmail.com>
2026-02-03 14:05:20 +0200

    ok

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿const imageMap = {
+  radio: "images/radio.png",
+  microwave: "images/smartphone.png",
+  infrared: "images/heating_element.png",
+  visible: "images/bulb.png",
+  ultraviolet: "images/sun.png",
+  xray: "images/xray.png"
+};
+
+const radioButtons = document.querySelectorAll('input[name="spectrum"]');
+const canvasImage = document.querySelector(".canvas-image");
+
+function updateImage(value) {
+  const src = imageMap[value];
+  if (src) {
+    canvasImage.src = src;
+  }
+}
+
+radioButtons.forEach((btn) => {
+  btn.addEventListener("change", (e) => {
+    updateImage(e.target.value);
+  });
+});
+
+const selected = document.querySelector('input[name="spectrum"]:checked');
+if (selected) updateImage(selected.value);
